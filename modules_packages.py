1027cca471006434a06dff692bc33917e914cd98
Pankaj Kumar <137532694+pythonwithpankaj@users.noreply.github.com>
2023-10-06 11:46:36 +0530

    Add files via upload

---
+++ modules_packages.py
@@ -0,0 +1,49 @@
+## Modules  --  python file
+## Packages --  collection of modules
+
+
+## Types of modules and packages
+                       # in built
+                       # user defined
+
+
+import random
+import time
+import os
+import sys
+
+# import random
+# print(random.randint(1,10))
+
+# print(random())
+# print(randint(1,10))
+
+
+# print(time.time())
+# print(time.asctime())                   ## checked current time
+
+
+# from urllib import request                                    #1st
+# data=request.urlopen("https://www.google.com/")
+# print(data)
+
+# from urllib.request   urlopen                            #2nd
+# data=urlopen("https://www.google.com/")
+
+
+# import collections
+# print(collections.Counter("python"))
+
+
+# import functions_                                     #1st
+# print(functions_.check_prime(6))
+
+# from functions_ import check_prime as cp              #2nd
+# cp(6)
+
+
+# import Python
+# from python import oop
+ 
+# obj1=oop.Institute("rohan","python")
+# python.display()
